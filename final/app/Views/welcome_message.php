f53d5361a7f3482079f29bfd2f2412dc878df4e5
DESKTOP-MULIPRV\banar <{jeoffgbanaria@gmail.com}>
2025-10-30 18:20:12 +0800

    guest

--- final/app/Views/welcome_message.php
+++ final/app/Views/welcome_message.php
@@ -12,24 +12,34 @@
 
     <div class="heroe">
 
-        <h1>Welcome <?= session()->get('username') ?></h1>
+        <?php if(!session()->get('isLoggedIn')): ?>
 
-        <h2>Your role is  <?= session()->get('role') ?></h2>
+            <h1>Welcome Guest</h1>
 
-        <?php if(session()->get('role') === 'admin'): ?>
-            <h1><a href="/list_user">List of User</a></h1>
+            <h2>Please  <a href="/login">login</a> to continue!</h2>
+
         
         <?php else: ?>
-             <h1><a href="<?= base_url('/edit_user/'.session()->get('user_id')) ?>">Edit My Information</a></h1>
-        <?php endif; ?>
 
-        <button><a href="/logout">LOGOUT</a></button>
-    </div>
+            <h1>Welcome <?= session()->get('username') ?></h1>
 
-    <div>
-        <h2>Manage Patient</h2>
-    </div>
+            <h2>Your role is  <?= session()->get('role') ?></h2>
 
+            <?php if(session()->get('role') === 'admin'): ?>
+                <h1><a href="/list_user">List of User</a></h1>
+            
+            <?php else: ?>
+                <h1><a href="<?= base_url('/edit_user/'.session()->get('user_id')) ?>">Edit My Information</a></h1>
+            <?php endif; ?>
+
+            <button><a href="/logout">LOGOUT</a></button>
+            </div>
+                    <br><br><br>
+            <div>
+                <button><a href="#">Manage Patient</a></button>
+            </div>
+            
+        <?php endif; ?>    
 </header>
 
 
