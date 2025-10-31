6559127afc3c3a81bd6f381c20c151e2815b1ebe
Jeoff Banaria <jeoffgbanaria@gmail.com>
2025-10-31 11:32:57 +0800

    x- user -> patient

---
+++ final/app/Controllers/AppointmentController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+use CodeIgniter\HTTP\ResponseInterface;
+
+class AppointmentController extends BaseController
+{
+    public function index()
+    {
+        //
+    }
+}
