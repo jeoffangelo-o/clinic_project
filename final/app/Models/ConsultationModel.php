184257a16403b9b8f75c75182147fdbb88805d2e
Jeoff Banaria <jeoffgbanaria@gmail.com>
2025-11-01 09:55:17 +0800

    consult start

--- final/app/Models/ConsultationModel.php
+++ final/app/Models/ConsultationModel.php
@@ -7,40 +7,16 @@ use CodeIgniter\Model;
 class ConsultationModel extends Model
 {
     protected $table            = 'consultations';
-    protected $primaryKey       = 'id';
-    protected $useAutoIncrement = true;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $primaryKey       = 'consultation_id';
+    
+    protected $allowedFields    = ['appointment_id',
+                                'patient_id',
+                                'nurse_id',
+                                'diagnosis',
+                                'treatment',
+                                'prescription',
+                                'consultation_date',
+                                'notes'
+                                ];
 
-    protected bool $allowEmptyInserts = false;
-    protected bool $updateOnlyChanged = true;
-
-    protected array $casts = [];
-    protected array $castHandlers = [];
-
-    // Dates
-    protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
 }
