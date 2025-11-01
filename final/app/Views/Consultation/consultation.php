bbbb5bea5efa5abaafec875f3f1e1959ada413e6
Jeoff Banaria <jeoffgbanaria@gmail.com>
2025-11-01 18:49:33 +0800

    main

--- final/app/Views/Consultation/consultation.php
+++ final/app/Views/Consultation/consultation.php
@@ -8,9 +8,23 @@
 <body>
     <h1>Consultation</h1>
 
-    <button><a href="/consultation/add">Add Consultation</a></button>
     
+    <button><a href="/consultation/add">Add Consultation</a></button>
 
+    <?php foreach($consult as $c): ?>
+
+    <div style="border: 1px solid black;">
+        <p><strong>Appointment ID:</strong> <?= ($c['appointment_id'] === null) ? $c['appointment_id'] : 'N/A' ?> </p>
+        <p><strong>Patient ID:</strong> <?= $c['patient_id'] ?> </p>
+        <p><strong>Nurse ID:</strong> <?= $c['nurse_id'] ?> </p>
+        <p><strong>Diagnosis:</strong> <?= $c['diagnosis'] ?> </p>
+        <p><strong>Treatment:</strong> <?= $c['treatment'] ?> </p>
+        <p><strong>Prescription:</strong> <?= $c['prescription'] ?> </p>
+        <p><strong>Notes:</strong> <?= $c['notes'] ?> </p>
+        <p><strong>Consultation Date:</strong> <?= $c['consultation_date'] ?> </p>
+    </div>
+
+    <?php endforeach; ?>
 
 </body>
 </html>
