ec24850c25d2742e781f06215e980c1007f4edc5
Jeoff Banaria <jeoffgbanaria@gmail.com>
2025-11-01 18:54:25 +0800

    comp

--- final/app/Views/Consultation/consultation.php
+++ final/app/Views/Consultation/consultation.php
@@ -11,20 +11,27 @@
     
     <button><a href="/consultation/add">Add Consultation</a></button>
 
-    <?php foreach($consult as $c): ?>
+    <?php if(!empty($consult)): ?>
 
-    <div style="border: 1px solid black;">
-        <p><strong>Appointment ID:</strong> <?= ($c['appointment_id'] === null) ? $c['appointment_id'] : 'N/A' ?> </p>
-        <p><strong>Patient ID:</strong> <?= $c['patient_id'] ?> </p>
-        <p><strong>Nurse ID:</strong> <?= $c['nurse_id'] ?> </p>
-        <p><strong>Diagnosis:</strong> <?= $c['diagnosis'] ?> </p>
-        <p><strong>Treatment:</strong> <?= $c['treatment'] ?> </p>
-        <p><strong>Prescription:</strong> <?= $c['prescription'] ?> </p>
-        <p><strong>Notes:</strong> <?= $c['notes'] ?> </p>
-        <p><strong>Consultation Date:</strong> <?= $c['consultation_date'] ?> </p>
-    </div>
+        <?php foreach($consult as $c): ?>
 
-    <?php endforeach; ?>
+            <div style="border: 1px solid black;">
+                <p><strong>Appointment ID:</strong> <?= ($c['appointment_id'] === null) ? $c['appointment_id'] : 'N/A' ?> </p>
+                <p><strong>Patient ID:</strong> <?= $c['patient_id'] ?> </p>
+                <p><strong>Nurse ID:</strong> <?= $c['nurse_id'] ?> </p>
+                <p><strong>Diagnosis:</strong> <?= $c['diagnosis'] ?> </p>
+                <p><strong>Treatment:</strong> <?= $c['treatment'] ?> </p>
+                <p><strong>Prescription:</strong> <?= $c['prescription'] ?> </p>
+                <p><strong>Notes:</strong> <?= $c['notes'] ?> </p>
+                <p><strong>Consultation Date:</strong> <?= $c['consultation_date'] ?> </p>
+            </div>
 
+        <?php endforeach; ?>
+    
+    <?php else: ?>
+
+        <h3>No Consultationss Yet</h3>
+
+    <?php endif; ?>
 </body>
 </html>
