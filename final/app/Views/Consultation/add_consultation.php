d89e3a8e0989700904e1cff0a0d6d89058315613
Jeoff Banaria <jeoffgbanaria@gmail.com>
2025-11-03 19:26:44 +0800

    2.0

--- final/app/Views/Consultation/add_consultation.php
+++ final/app/Views/Consultation/add_consultation.php
@@ -7,5 +7,30 @@
 </head>
 <body>
     <h1>Add Consultation</h1>
+
+    <?php if(session()->get('role') === 'admin' || session()->get('role')):  ?>
+
+        <form action="/consultation" method="get" >
+            <label for="">Service:</label>
+            <select name="service" id="" onchange="this.form.submit()">
+                <option value="walkin">Walk-In</option>
+                <option value="appoint">Appointment</option>
+            </select>
+        </form>
+        
+        <form action="/consultation/add" method="post">
+
+            <?php if(session()->get('walkIn')): ?>
+
+            <?php else: ?>
+                
+            <?php endif; ?>
+
+        </form>
+
+    <?php else: ?>
+
+    <?php endif; ?>
+
 </body>
 </html>
