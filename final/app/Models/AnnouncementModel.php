aa3e1343203b92ef6ee2d5cf68e0e24a436f00bb
Jeoff Banaria <jeoffgbanaria@gmail.com>
2025-11-04 18:06:20 +0800

    announce

--- final/app/Models/AnnouncementModel.php
+++ final/app/Models/AnnouncementModel.php
@@ -7,40 +7,12 @@ use CodeIgniter\Model;
 class AnnouncementModel extends Model
 {
     protected $table            = 'announcements';
-    protected $primaryKey       = 'id';
-    protected $useAutoIncrement = true;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $primaryKey       = 'announcement_id';
+  
+    protected $allowedFields    = ['title',
+                                    'content',
+                                    'posted_by',
+                                    'posted_at'
+                                    ];
 
-    protected bool $allowEmptyInserts = false;
-    protected bool $updateOnlyChanged = true;
-
-    protected array $casts = [];
-    protected array $castHandlers = [];
-
-    // Dates
-    protected $useTimestamps = false;
-    protected $dateFormat    = 'datetime';
-    protected $createdField  = 'created_at';
-    protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
 }
