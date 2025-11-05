a2ed65237cd30a2c18d52dfc998109323f8ca3bc
Jeoff Banaria <jeoffgbanaria@gmail.com>
2025-11-06 07:56:56 +0800

    2

--- final/app/Views/Announcement/add_announcement.php
+++ final/app/Views/Announcement/add_announcement.php
@@ -3,18 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Add Announcement</title>
+    <title>Create Announcement</title>
 </head>
 <body>
-    <h1>Add Announcement</h1>
+    <h1>Create Announcement</h1>
 
     <form action="/announcement/add" method="post">
         <label for="">Title:</label>
         <input type="text" name="title" id=""><br><br>
         <label for="">Content:</label>
         <textarea name="content" id=""></textarea><br><br>
-        <label for="">Title:</label>
-        <input type="text" name="title" id=""><br><br>
+        <label for="">Posted Until:</label>
+        <input type="datetime-local" name="posted_until" id="">
+        <input type="submit" value="Create Announcement">
+        
     </form>
 </body>
 </html>
